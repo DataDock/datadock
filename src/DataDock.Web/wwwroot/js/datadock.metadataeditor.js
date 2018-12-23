6a0dd672d32dc45f428478147bc781fba2bca2c9
Kal Ahmed <techquila@gmail.com>
2018-12-23 15:01:41 +0000

    Revert logic for generating aboutUrl (#98)
    * Change logic for column-based aboutUrl
    
    If the aboutUrl uses a column rather than the default row number, then don't include the datasetId in the aboutUrl template. This allows for different CSV files using the same identifier column to automatically join.
    Fixes #94
    
    * Treat dashes and underscores as word separates when camelizing
    
    * Use development environment for running web image locally

--- src/DataDock.Web/wwwroot/js/datadock.metadataeditor.js
+++ src/DataDock.Web/wwwroot/js/datadock.metadataeditor.js
@@ -153,8 +153,7 @@ var templateMetadata;
 
         csvw["dc:license"] = $("#datasetLicense").val();
 
-        var suffix = $("#aboutUrlSuffix").val();
-        var aboutUrl = datasetId.replace("/dataset/", "/resource/") + "/" + suffix;
+        var aboutUrl = $('#aboutUrlSuffix').val();
         csvw["aboutUrl"] = aboutUrl;
 
         csvw["tableSchema"] = constructCsvwtableSchema();
@@ -631,7 +630,8 @@ var templateMetadata;
 
     function constructIdentifiersTabContent() {
         var prefix = getPrefix();
-        var idFromFilename = prefix + "/id/dataset/" + slugify(filename, "", "", "camelCase");
+        var datasetId = slugify(filename, "", "", "camelCase");
+        var idFromFilename = prefix + "/id/dataset/" + datasetId;
         var defaultValue = getMetadataDatasetId(idFromFilename);
 
         var dsIdTable = {
@@ -701,14 +701,15 @@ var templateMetadata;
                 ]
             }
         );
-        var rowIdentifier = "row_{_row}";
+
+        var rowIdentifier = getIdentifierPrefix() + "/" + datasetId + "/row_{_row}";
         var identifierOptions = {};
         identifierOptions[rowIdentifier] = "Row Number";
 
         for (var colIdx = 0; colIdx < columnCount; colIdx++) {
             var colTitle = header[colIdx];
             var colName = slugify(colTitle, "_", "_", "lowercase");
-            var colIdentifier = colName + "/{" + colName + "}";
+            var colIdentifier = getIdentifierPrefix() + "/" + colName + "/{" + colName + "}";
             identifierOptions[colIdentifier] = colTitle;
         }
         identifierTableElements.push(
@@ -1081,6 +1082,10 @@ var templateMetadata;
         return publishUrl + "/" + ownerId + "/" + repoId;
     }
 
+    function getIdentifierPrefix() {
+        return getPrefix() + "/id/resource";
+    }
+
     function slugify(original, whitespaceReplacement, specCharReplacement, casing) {
         switch (casing) {
         case "lowercase":
@@ -1100,7 +1105,7 @@ var templateMetadata;
     }
 
     function camelize(str) {
-        var camelised = str.split(" ").map(function(word, index) {
+        var camelised = str.split(/[\s_\-]/).map(function(word, index) {
             if (index === 0) {
                 return word.toLowerCase();
             }
