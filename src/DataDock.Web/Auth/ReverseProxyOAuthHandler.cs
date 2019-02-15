41c759da0b9b77a90138033f9b671914dca55e16
Kal Ahmed <kal@networkedplanet.com>
2019-02-15 16:21:54 +0000

    Fix for getting authentication token

--- src/DataDock.Web/Auth/ReverseProxyOAuthHandler.cs
+++ src/DataDock.Web/Auth/ReverseProxyOAuthHandler.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
@@ -7,6 +11,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace DataDock.Web.Auth
 {
@@ -45,6 +50,44 @@ namespace DataDock.Web.Auth
                 ? new HostString(Request.Headers["X-Forwarded-Host"][0])
                 : Request.Host;
             return schema + "://" + host + OriginalPathBase + targetPath;
+        }
+
+        protected virtual async Task<OAuthTokenResponse> ExchangeCodeAsync(string code, string redirectUri)
+        {
+            var tokenRequestParameters = new Dictionary<string, string>()
+            {
+                {"client_id", Options.ClientId},
+                {"redirect_uri", redirectUri},
+                {"client_secret", Options.ClientSecret},
+                {"code", code},
+                {"grant_type", "authorization_code"},
+            };
+
+            var requestContent = new FormUrlEncodedContent(tokenRequestParameters);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, Options.TokenEndpoint);
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            requestMessage.Content = requestContent;
+            var response = await Backchannel.SendAsync(requestMessage, Context.RequestAborted);
+            if (response.IsSuccessStatusCode)
+            {
+                var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+                return OAuthTokenResponse.Success(payload);
+            }
+            else
+            {
+                var error = "OAuth token endpoint failure: " + await Display(response);
+                return OAuthTokenResponse.Failed(new Exception(error));
+            }
+        }
+
+        private static async Task<string> Display(HttpResponseMessage response)
+        {
+            var output = new StringBuilder();
+            output.Append("Status: " + response.StatusCode + ";");
+            output.Append("Headers: " + response.Headers.ToString() + ";");
+            output.Append("Body: " + await response.Content.ReadAsStringAsync() + ";");
+            return output.ToString();
         }
     }
 }
