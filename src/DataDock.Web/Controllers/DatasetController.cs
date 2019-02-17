e90498c84a0deae44e4c0bcaa3e128d316c19e41
Kal Ahmed <kal@networkedplanet.com>
2019-02-17 16:55:47 +0000

    Delete action now starts a delete job

--- src/DataDock.Web/Controllers/DatasetController.cs
+++ src/DataDock.Web/Controllers/DatasetController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using DataDock.Common;
+using DataDock.Common.Models;
 using DataDock.Common.Stores;
 using DataDock.Web.Auth;
 using DataDock.Web.Models;
@@ -6,16 +8,21 @@ using DataDock.Web.Routing;
 using DataDock.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace DataDock.Web.Controllers
 {
     public class DatasetController : DashboardBaseController
     {
-        private IDatasetStore _datasetStore;
+        private readonly IDatasetStore _datasetStore;
+        private readonly IJobStore _jobStore;
+        private readonly IDataDockUriService _uriService;
 
-        public DatasetController(IDatasetStore datasetStore)
+        public DatasetController(IDatasetStore datasetStore, IJobStore jobStore, IDataDockUriService uriService)
         {
             _datasetStore = datasetStore;
+            _jobStore = jobStore;
+            _uriService = uriService;
         }
 
 
@@ -61,26 +68,42 @@ namespace DataDock.Web.Controllers
         [ServiceFilter(typeof(OwnerAdminAuthFilter))]
         public async Task<IActionResult> DeleteDataset(string ownerId, string repoId, string datasetId, bool confirmed = false)
         {
-            this.DashboardViewModel.Area = "datasets";
+            DashboardViewModel.Area = "datasets";
             DashboardViewModel.SelectedDatasetId = datasetId;
-            DashboardViewModel.Heading = string.Format("Delete Dataset: {0}", datasetId);
+            DashboardViewModel.Heading = $"Delete Dataset: {datasetId}";
             if (!confirmed)
             {
                 return View("Dashboard/DeleteDataset", this.DashboardViewModel);
             }
-            else
+
+            // Validate user name and authentication status
+            var userId = User?.Identity?.Name;
+            if (string.IsNullOrEmpty(userId))
             {
-                if (await _datasetStore.DeleteDatasetAsync(ownerId, repoId, datasetId))
-                {
-                    TempData["message"] = $"Dataset {datasetId} successfully deleted.";
-                    return RedirectToRoute("RepoDatasets", new {ownerId, repoId});
-                }
-                else
-                {
-                    ViewBag.StatusMessage = "Failed to delete dataset {datasetId}";
-                    return View("Dashboard/DeleteDataset", this.DashboardViewModel);
-                }
+                Log.Debug("DeleteDataset: No value found for user principal name");
+                return Unauthorized();
             }
+
+            if (User?.Identity?.IsAuthenticated != true)
+            {
+                Log.Debug("DeleteDataset: User identity is not authenticated");
+                return Unauthorized();
+            }
+
+            var jobInfo = await _jobStore.SubmitDeleteJobAsync(new DeleteJobRequestInfo
+            {
+                UserId = userId,
+                OwnerId = ownerId,
+                RepositoryId = repoId,
+                DatasetId = datasetId,
+                DatasetIri = _uriService.GetDatasetIdentifier(ownerId, repoId, datasetId)
+            });
+            if (jobInfo != null)
+            {
+                return RedirectToRoute("RepoJobs", new {ownerId, repoId, jobInfo.JobId});
+            }
+            ViewBag.StatusMessage = "Failed to delete dataset {datasetId}";
+            return View("Dashboard/DeleteDataset", this.DashboardViewModel);
         }
     }
 }
